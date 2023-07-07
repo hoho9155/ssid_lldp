bb82ad101784c3445d35051750ba469d4c5a8360
hoho <hoho915571@gmail.com>
2023-07-07 09:35:11 -0700

    try-catch

--- Program.cs
+++ Program.cs
@@ -3,17 +3,24 @@
 
 //SSID
 Console.WriteLine("SSID List:");
-String query = "SELECT * FROM MSNDis_80211_BSSIList";
-ManagementObjectSearcher searcher = new ManagementObjectSearcher("root/WMI", query);
-ManagementObjectCollection moc = searcher.Get();
-ManagementObjectCollection.ManagementObjectEnumerator moe = moc.GetEnumerator();
-moe.MoveNext();
-ManagementBaseObject[] objarr = (ManagementBaseObject[])moe.Current.Properties["Ndis80211BSSIList"].Value;
-foreach (ManagementBaseObject obj in objarr)
+try
 {
-    uint u_ssid = (uint)obj["Ndis80211Ssid"];
-    int ssid = (int)u_ssid;
-    Console.WriteLine($"SSID: {ssid}");
+    String query = "SELECT * FROM MSNDis_80211_BSSIList";
+    ManagementObjectSearcher searcher = new ManagementObjectSearcher("root/WMI", query);
+    ManagementObjectCollection moc = searcher.Get();
+    ManagementObjectCollection.ManagementObjectEnumerator moe = moc.GetEnumerator();
+    moe.MoveNext();
+    ManagementBaseObject[] objarr = (ManagementBaseObject[])moe.Current.Properties["Ndis80211BSSIList"].Value;
+    foreach (ManagementBaseObject obj in objarr)
+    {
+        uint u_ssid = (uint)obj["Ndis80211Ssid"];
+        int ssid = (int)u_ssid;
+        Console.WriteLine($"SSID: {ssid}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
 }
 
 //LLDP
